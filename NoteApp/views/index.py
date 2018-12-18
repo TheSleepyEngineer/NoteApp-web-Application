69c9089aa3f2998aa7ccbf9d5cc58715b0ca489e
mahmoud <mahmoud.odb@gmail.com>
2018-12-17 23:19:01 -0500

    notes stored in files

--- NoteApp/views/index.py
+++ NoteApp/views/index.py
@@ -1,7 +1,25 @@
 from flask import Blueprint, render_template
+import glob
 
 bp = Blueprint(__name__, __name__,template_folder='../templates')
 
+
+def fetch_notes():
+    final_notes= []
+    notes=glob.glob('NoteApp/notes/*.note')
+
+    for note in notes:
+        with open(note) as _file:
+            final_notes.append(_file.read())
+        _file.close()
+
+    return final_notes
+
+
+    return notes
+
+
+
 @bp.route('/')
 def show():
-    return render_template('index.html')
+    return render_template('index.html', notes=fetch_notes())
